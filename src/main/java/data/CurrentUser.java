002947d6e622ae49a6bcced5e89fa2d640bf6f59
Alina <alinablago19@gmail.com>
2020-09-14 23:38:14 +0300

    Added classes Message and fixed POJO classes in package data

--- src/main/java/data/CurrentUser.java
+++ src/main/java/data/CurrentUser.java
@@ -1,4 +1,30 @@
 package data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CurrentUser {
+    private static User currentUser;
+    private static String currentKey;
+    public static Thread ourThread ;
+    public static String currentChat;
+    private static final Logger logger = LoggerFactory.getLogger(CurrentUser.class);
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static String getCurrentKey() {
+        return currentKey;
+    }
+
+    public static void Init(User user , String key){
+        currentUser = user;
+        currentKey = key;
+    }
+
+    public static void LogOut(){
+        currentKey = "";
+        currentUser = null;
+    }
 }
--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -1,4 +1,66 @@
 package data;
 
 public class User {
+    private static int NextID;
+    private int ID;
+    private String Name;
+    private String Login;
+    private String Password;
+    private boolean isBanned;
+
+    public User(String name, String login, String password) {
+        Name = name;
+        Login = login;
+        Password = password;
+        this.isBanned = false;
+    }
+
+    public static int getNextID() {
+        NextID += 1;
+        return NextID;
+    }
+
+    public static void setNextID(int nextID) {
+        NextID = nextID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String login) {
+        Login = login;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public boolean isBanned() {
+        return isBanned;
+    }
+
+    public void setBanned(boolean banned) {
+        isBanned = banned;
+    }
 }
